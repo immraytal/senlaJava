98bb28ae53457c4da9f64303970d655cc8225675
Surv <mega.andrej2013@gmail.com>
2019-11-07 17:45:17 +0300

    init 1 task OK 2 task OK

--- src/task2/Second.java
+++ src/task2/Second.java
@@ -9,14 +9,47 @@ import java.util.Scanner;
 
 public class Second {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите два целых числа: ");
-        if (scanner.hasNextInt()) {
-            int number = scanner.nextInt();
-
-        } else {
+        int num1, num2;
+        try {
+            System.out.print("Введите 2 целых числа через пробел: ");
+            num1 = scanner.nextInt();
+            num2 = scanner.nextInt();
+        } catch (Exception e) {
             System.out.println("Числа не определены, проверьте введённые данные");
+            return;
         }
+        System.out.println(NOD(num1,num2)+" "+NOK(num1,num2));
+    }
+    private static int NOD(int num1,int num2) {
+        int min;
+        int nod = 0;
+        if(num1 > num2)
+            min = num2;
+        else
+            min = num1;
+
+        for (int count = 1; count <= min; count++) {
+            if (num1 % count == 0 && num2 % count == 0){
+                if (count>nod)
+                    nod = count;
+            }
+        }
+        return nod;
+    }
+    private static int NOK(int num1,int num2) {
+        int count;
+        int nok = 0;
+        if (num1 < num2)
+            count = num2;
+        else
+            count = num1;
+
+        while (nok == 0) {
+            if (count % num1 == 0 && count % num2 == 0)
+                nok = count;
+            count++;
+        }
+        return nok;
     }
 }
